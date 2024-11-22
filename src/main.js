3bb0f580bbbfb0963d320f6154afd802ac00d0b0
Julio Silva <silvajulio@hotmail.com>
2024-11-21 23:48:37 -0500

    Adição das perguntas / respostas

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
     const Buttons = document.querySelectorAll('[data-tab-button]');
+    const QuestionButtonClose = document.querySelectorAll('[data-btn-close]');
+    const QuestionButtonOpen = document.querySelectorAll('[data-btn-open]');
 
     for (let i = 0; i < Buttons.length; i++) {
         Buttons[i].addEventListener('click', function(botao) {
@@ -12,6 +14,38 @@ document.addEventListener('DOMContentLoaded', function() {
             const Button = document.querySelector(`[data-tab-button=${TabTarget}]`);
             ButtonHideAll();
             Button.classList.add('shows__tabs__button--is-active');
+        })
+    }
+
+    for (let i = 0; i < QuestionButtonClose.length; i++) {
+        QuestionButtonClose[i].addEventListener('click', function(botao) {
+            const TargetNumber = botao.target.dataset.btnClose;
+
+            const AnswerBox = document.querySelector(`[data-answer-box=${TargetNumber}]`);
+            AnswerBox.classList.remove('faq__questions__item__answer-opened');
+            AnswerBox.classList.add('faq__questions__item__answer-closed');
+
+            const AnswerCloseSign = document.querySelector(`[data-btn-close=${TargetNumber}]`);
+            AnswerCloseSign.style.display = 'none';
+
+            const AnswerOpenSign = document.querySelector(`[data-btn-open=${TargetNumber}]`);
+            AnswerOpenSign.style.display = '';
+        })
+    }
+
+    for (let i = 0; i < QuestionButtonOpen.length; i++) {
+        QuestionButtonOpen[i].addEventListener('click', function(botao) {
+            const TargetNumber = botao.target.dataset.btnOpen;
+
+            const AnswerBox = document.querySelector(`[data-answer-box=${TargetNumber}]`);
+            AnswerBox.classList.remove('faq__questions__item__answer-closed');
+            AnswerBox.classList.add('faq__questions__item__answer-opened');
+
+            const AnswerCloseSign = document.querySelector(`[data-btn-close=${TargetNumber}]`);
+            AnswerCloseSign.style.display = '';
+
+            const AnswerOpenSign = document.querySelector(`[data-btn-open=${TargetNumber}]`);
+            AnswerOpenSign.style.display = 'none';
         })
     }
 })
